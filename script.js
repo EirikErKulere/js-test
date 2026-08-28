759d1210d4b6d7379849512875b4ef4215d81b94
Eirik <eirikgf08@gmail.com>
2026-08-28 11:07:24 +0200

    Sitater

--- script.js
+++ script.js
@@ -5,7 +5,19 @@ document.querySelector(".theme-btn").addEventListener("click", () => {
 });
 
 let players = [];
-
 document.querySelector(".add-player-btn").addEventListener("click", () => {
     players.push(new Player("Player " + (players.length+1)));
+});
+
+const legendaryQuotes = [
+    "Jeg bruker Osloskolen-GPT hver dag!",
+    "Osloskolen-GPT elsker deg! ❤️",
+    "why do they call it oven when you of in the cold food of out hot eat the food"
+];
+let currentQuoteIndex = 1;
+document.querySelector(".quote-btn").addEventListener("click", () => {
+    let quoteIndex = Math.floor(Math.random() * (legendaryQuotes.length - 1));
+    if (quoteIndex >= currentQuoteIndex) quoteIndex++;
+    currentQuoteIndex = quoteIndex;
+    document.querySelector(".quote").textContent = legendaryQuotes[quoteIndex];
 });
